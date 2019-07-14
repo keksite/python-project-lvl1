3deeb0c5946344578159089409db72e6a93eeed8
keksite <keksite@mail.ru>
2019-07-14 12:37:51 +0300

    complete second task

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-   
-def main():
-	print("Welcome to the Brain Games!")
+from brain_games.cli import run
 
-if __name__ == "__main__":
-	main() 
+
+def main():
+	run()
+
+
+if __name__ == '__main__':
+	main()
